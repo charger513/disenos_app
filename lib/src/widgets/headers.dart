ade4b0b4e09588712031f7ee07ca352e94a3cae5
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-17 18:36:34 -0500

    Header Bordes Redondeados

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -9,3 +9,19 @@ class HeaderCuadrado extends StatelessWidget {
     );
   }
 }
+
+class HeaderBordesRedondeados extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: BoxDecoration(
+        color: Color(0xFF615AAB),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(70),
+          bottomRight: Radius.circular(70),
+        ),
+      ),
+    );
+  }
+}
