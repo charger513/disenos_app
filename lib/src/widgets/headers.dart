2419891fd9da6ca14970925a4ea45ba3b3f9d063
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-17 19:59:21 -0500

    CustomPainter con Gradiente

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -170,7 +170,8 @@ class _HeaderCurvoPainter extends CustomPainter {
 
     // Dibujar con el path y el lapiz (paint)
     path.lineTo(0, size.height * 0.25);
-    path.quadraticBezierTo(size.width * 0.5, size.height * 0.10, size.width, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width * 0.5, size.height * 0.10, size.width, size.height * 0.25);
     path.lineTo(size.width, 0);
 
     canvas.drawPath(path, paint);
@@ -209,8 +210,73 @@ class _HeaderWavesPainter extends CustomPainter {
 
     // Dibujar con el path y el lapiz (paint)
     path.lineTo(0, size.height * 0.25);
-    path.quadraticBezierTo(size.width * 0.25, size.height * 0.3, size.width * 0.5, size.height * 0.25);
-    path.quadraticBezierTo(size.width * 0.75, size.height * 0.2, size.width, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.3,
+        size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.2, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderWavesGradient extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderWavesGradientPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavesGradientPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final Rect rect = Rect.fromCircle(
+      center: Offset(0, 55),
+      radius: 180
+    );
+
+    final Gradient gradiente = LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: <Color>[
+        Color(0xff6d05e8),
+        Color(0xffc012ff),
+        Color(0xff6d05fa),
+      ],
+      stops: [
+        0.2,
+        0.5,
+        0.8,
+      ]
+    );
+
+    final paint = Paint()..shader = gradiente.createShader(rect);
+
+    // Propiedades
+    // paint.color = Color(0xFF615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 10;
+
+    final path = Path();
+
+    // Dibujar con el path y el lapiz (paint)
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.3,
+        size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.2, size.width, size.height * 0.25);
     path.lineTo(size.width, 0);
 
     canvas.drawPath(path, paint);
