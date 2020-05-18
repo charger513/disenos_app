7e9263be0296798ced58d169edc0d847bc9c73c8
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-17 20:15:34 -0500

    Preparando la pantalla para animar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:disenos_app/src/pages/animaciones_page.dart';
 import 'package:disenos_app/src/pages/headers_page.dart';
 import 'package:flutter/material.dart';
 
@@ -6,9 +7,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Diseños App',
-      home: HeadersPage()
-    );
+    return MaterialApp(title: 'Diseños App', home: AnimacionesPage());
   }
 }
---
+++ lib/src/pages/animaciones_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AnimacionesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Rectangulo(),
+      ),
+    );
+  }
+}
+
+class Rectangulo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
