60b98433f4a4a9e071f96fefe9b8b56c1da484b9
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-17 21:26:54 -0500

    Inicio de un nuevo reto

---
+++ lib/src/retos/cuadrado_animado_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CuadradoAnimadoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CuadradoAnimado(),
+      ),
+    );
+  }
+}
+
+class CuadradoAnimado extends StatefulWidget {
+  @override
+  _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
+}
+
+class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _Rectangulo();
+  }
+}
+
+class _Rectangulo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
