3a8d49f4c747e1edeeed054c4da4a093795eef22
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-17 20:23:55 -0500

    Animation y AnimationController

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -5,13 +5,47 @@ class AnimacionesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Rectangulo(),
+        child: CuadradoAnimado(),
       ),
     );
   }
 }
 
-class Rectangulo extends StatelessWidget {
+class CuadradoAnimado extends StatefulWidget {
+  @override
+  _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
+}
+
+class _CuadradoAnimadoState extends State<CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> rotacion;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 4000),
+    );
+
+    rotacion = Tween(begin: 0.0, end: 2.0).animate(controller);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _Rectangulo();
+  }
+}
+
+class _Rectangulo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
