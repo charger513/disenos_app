8358c9e1c001772e0e93529e092443329f802f1a
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-19 12:11:16 -0500

    CustomPainter - Círculos y Arcos

--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class CircularProgressPage extends StatefulWidget {
@@ -11,11 +13,48 @@ class _CircularProgressPageState extends State<CircularProgressPage> {
     return Scaffold(
       body: Center(
         child: Container(
+          padding: const EdgeInsets.all(5),
           width: 300,
           height: 300,
-          color: Colors.red,
+          child: CustomPaint(
+            painter: _MiRadialProgress(50),
+          ),
         ),
       ),
     );
   }
+}
+
+class _MiRadialProgress extends CustomPainter {
+  final porcentaje;
+
+  _MiRadialProgress(this.porcentaje);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Circulo completado
+    final paint = Paint()
+      ..strokeWidth = 4
+      ..color = Colors.grey
+      ..style = PaintingStyle.stroke;
+
+    final center = new Offset(size.width / 2, size.height / 2);
+    final radio = min(size.width / 2, size.height / 2);
+
+    canvas.drawCircle(center, radio, paint);
+
+    // Arco
+    final paintArco = Paint()
+      ..strokeWidth = 10
+      ..color = Colors.pink
+      ..style = PaintingStyle.stroke;
+
+    // Parte que se deberá ir llenando
+    double arcAngle = 2 * pi * (porcentaje / 100);
+
+    canvas.drawArc(Rect.fromCircle(center: center, radius: radio), -pi/2, arcAngle, false, paintArco);
+  }
+
+  @override
+  bool shouldRepaint(_MiRadialProgress oldDelegate) => true;
 }
