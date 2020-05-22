5da0d7c39861229cb87d42251b692092190b009c
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-21 19:50:31 -0500

    PageView - Creando un Slideshow

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -6,8 +6,39 @@ class SlideshowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset('assets/svgs/slide-1.svg'),
+        child: _Slides()
       ),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: <Widget>[
+          _Slide('assets/svgs/slide-1.svg'),
+          _Slide('assets/svgs/slide-2.svg'),
+          _Slide('assets/svgs/slide-3.svg'),
+        ],
+      )
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: const EdgeInsets.all(30),
+      child: SvgPicture.asset(svg),
     );
   }
 }
