d37c0d94a55d4e7ef8f5979bdd05a2b60d64a4ee
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-21 20:53:53 -0500

    Widget Slideshow - inicio de optimizaciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:disenos_app/src/pages/animaciones_page.dart';
-import 'package:disenos_app/src/labs/circular_progress_page.dart';
-import 'package:disenos_app/src/pages/graficas_circulares_page.dart';
-import 'package:disenos_app/src/pages/headers_page.dart';
+// import 'package:disenos_app/src/pages/animaciones_page.dart';
+// import 'package:disenos_app/src/labs/circular_progress_page.dart';
+// import 'package:disenos_app/src/pages/graficas_circulares_page.dart';
+// import 'package:disenos_app/src/pages/headers_page.dart';
+// import 'package:disenos_app/src/labs/slideshow_page.dart';
+// import 'package:disenos_app/src/retos/cuadrado_animado_page.dart';
 import 'package:disenos_app/src/pages/slideshow_page.dart';
-import 'package:disenos_app/src/retos/cuadrado_animado_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
