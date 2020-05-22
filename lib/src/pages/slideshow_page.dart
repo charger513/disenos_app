1931c8f2f1eb522892d66a51f87066324b2d29fc
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-21 20:25:33 -0500

    ChangeNotifierProvider - Instanciar nuestra clase

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -1,13 +1,18 @@
+import 'package:disenos_app/src/models/slider_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
 
 class SlideshowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[Expanded(child: _Slides()), _Dots()],
+    return ChangeNotifierProvider(
+      create: (_) => SliderModel(),
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            children: <Widget>[Expanded(child: _Slides()), _Dots()],
+          ),
         ),
       ),
     );
@@ -39,11 +44,18 @@ class _Dot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final pageViewIndex = Provider.of<SliderModel>(context).currentPage;
+
     return Container(
-        width: 12,
-        height: 12,
-        margin: const EdgeInsets.symmetric(horizontal: 5),
-        decoration: BoxDecoration(color: Colors.grey, shape: BoxShape.circle));
+      width: 12,
+      height: 12,
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        color: pageViewIndex == index ? Colors.blue : Colors.grey,
+        shape: BoxShape.circle,
+      ),
+    );
   }
 }
 
@@ -59,7 +71,11 @@ class __SlidesState extends State<_Slides> {
   void initState() {
     super.initState();
     pageViewController.addListener(() {
-      print('Página actual ${pageViewController.page}');
+      // print('Página actual ${pageViewController.page}');
+
+      // Actualizar el provider, SliderModel
+      Provider.of<SliderModel>(context, listen: false).currentPage =
+          pageViewController.page;
     });
   }
 
