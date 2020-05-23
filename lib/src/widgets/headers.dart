a32bdb1b4c726f32b553d1c19631c1f1f54c468b
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-23 18:13:19 -0500

    Iconos y textos de mi IconHeader

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HeaderCuadrado extends StatelessWidget {
   @override
@@ -284,6 +285,62 @@ class _HeaderWavesGradientPainter extends CustomPainter {
 }
 
 class IconHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Color colorBlanco = Colors.white.withOpacity(0.7);
+
+    return Stack(
+      children: <Widget>[
+        _IconHeaderBackground(),
+        Positioned(
+          top: -50,
+          left: -70,
+          child: FaIcon(
+            FontAwesomeIcons.plus,
+            size: 250,
+            color: Colors.white.withOpacity(0.2),
+          ),
+        ),
+        Column(
+          children: <Widget>[
+            SizedBox(
+              height: 80,
+              width: double.infinity,
+            ),
+            Text(
+              'Has solicitado',
+              style: TextStyle(
+                fontSize: 20,
+                color: colorBlanco,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Asistenia Médica',
+              style: TextStyle(
+                fontSize: 25,
+                color: colorBlanco,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            FaIcon(
+              FontAwesomeIcons.plus,
+              size: 80,
+              color: Colors.white,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _IconHeaderBackground extends StatelessWidget {
+  const _IconHeaderBackground({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
