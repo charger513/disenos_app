50e1c3ef3a3ff139b80e1f43d34a1d8a8c10b97b
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-23 18:36:12 -0500

    Creando los botones

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,3 +1,4 @@
+import 'package:disenos_app/src/widgets/boton_gordo.dart';
 import 'package:disenos_app/src/widgets/headers.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -6,11 +7,22 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IconHeader(
-        icon: FontAwesomeIcons.plus,
-        titulo: 'Asistencia Médica',
-        subtitulo: 'Has solicitado',
-      ),
+      body: Center(child: BotonGordo())
+    );
+  }
+}
+
+class PageHeader extends StatelessWidget {
+  const PageHeader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconHeader(
+      icon: FontAwesomeIcons.plus,
+      titulo: 'Asistencia Médica',
+      subtitulo: 'Has solicitado',
     );
   }
 }
