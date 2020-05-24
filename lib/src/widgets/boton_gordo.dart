a040b7f3580e6ac47b666cc39cf5315f18273486
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-23 20:01:16 -0500

    Información dentro del botón

--- lib/src/widgets/boton_gordo.dart
+++ lib/src/widgets/boton_gordo.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BotonGordo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _BotonGordoBackground();
+    return Stack(
+      children: <Widget>[
+        _BotonGordoBackground(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 140,
+              width: 40,
+            ),
+            FaIcon(
+              FontAwesomeIcons.carCrash,
+              color: Colors.white,
+              size: 40,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: Text(
+                'Motor Accident',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+            FaIcon(FontAwesomeIcons.chevronRight, color: Colors.white),
+            SizedBox(width: 40,)
+          ],
+        ),
+      ],
+    );
   }
 }
 
@@ -11,19 +41,35 @@ class _BotonGordoBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              right: -20,
+              top: -20,
+              child: FaIcon(
+                FontAwesomeIcons.carCrash,
+                size: 150,
+                color: Colors.white.withOpacity(0.2),
+              ),
+            ),
+          ],
+        ),
+      ),
       width: double.infinity,
       height: 100,
       margin: const EdgeInsets.all(20),
       decoration: BoxDecoration(
-        boxShadow: <BoxShadow>[
-          BoxShadow(color: Colors.black.withOpacity(0.2), offset: Offset(4,6), blurRadius: 10)
-        ],
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(colors: [
-          Color(0xff6989f5),
-          Color(0xff906ef5)
-        ])
-      ),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                offset: Offset(4, 6),
+                blurRadius: 10)
+          ],
+          borderRadius: BorderRadius.circular(15),
+          gradient:
+              LinearGradient(colors: [Color(0xff6989f5), Color(0xff906ef5)])),
     );
   }
 }
